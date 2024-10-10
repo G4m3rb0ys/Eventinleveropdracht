72a4b89549110ea54ec5ece3d63a5c8a48afd1fd
Tim Tigelaar <145434414+G4m3rb0ys@users.noreply.github.com>
2024-10-10 10:40:20 +0200

    Added the organzier and events

--- Eventinleveropdracht/Models/User.cs
+++ Eventinleveropdracht/Models/User.cs
@@ -16,5 +16,6 @@ namespace Eventinleveropdracht.Models
         public string Username { get; set; }
         public string Password { get; set; }
         public string Role { get; set; }
+        public string Discriminator { get; set; }
     }
 }
